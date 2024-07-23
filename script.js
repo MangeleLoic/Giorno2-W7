057c074005b04d4eff5a72c208be990dec3d82f5
sudonona <80674485+sudonona@users.noreply.github.com>
2024-07-23 16:33:42 +0200

    up

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const saveBtn = document.getElementById('save');
 const resetBtn = document.getElementById('reset');
 const textArea = document.getElementById('input-textArea');
+const showUser = document.getElementById('savedUser')
+const Conteggio = document.getElementById('timer')
 
 const save = function () {
     const textAreaValue = textArea.value
-    localStorage.setItem('textContent', textAreaValue);
+    localStorage.setItem('username', textAreaValue);
 
 }
 saveBtn.addEventListener('click', save);
 
+const show = function() {
+
+    const savedValue = localStorage.getItem('username');
+    showUser.innerText = savedValue ? savedValue : '';
+
+}
+
+saveBtn.addEventListener('click', show);
+
 
 const reset = function () {
     textArea.value = '';
-    localStorage.removeItem('textContent');
+    localStorage.removeItem('username');
 };
 
 resetBtn.addEventListener('click', reset);
 
+
+
+function cronometro() {
+    let timer = JSON.parse(sessionStorage.getItem('timer'));
+    Conteggio.innerText = timer;
+    timer++;
+    sessionStorage.setItem('timer', timer);
+}
+
+setInterval(cronometro, 1000);
+cronometro();
